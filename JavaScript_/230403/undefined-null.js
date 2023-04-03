749af7b14b923dc77b88d1684e27fc47872a83da
Oh Ga Eun <dhrkdms4113@naver.com>
2023-04-03 16:07:09 +0900

    undefined and null

---
+++ JavaScript_/230403/undefined-null.js
@@ -0,0 +1,15 @@
+////// undefined : 값이 할당되어 있지 않음을 나타냄
+let a;
+console.log(a);
+
+if (typeof y === "undefined") {
+  console.log("y에 아무것도 할당되지 않았습니다.");
+}
+
+if (!a) {
+  console.log("y에 아무것도 할당되지 않았습니다.");
+}
+
+////// null : 값이 없음을 나타냄 (비어있음을 명시적으로 표현)
+let value = null;
+console.log(value);
