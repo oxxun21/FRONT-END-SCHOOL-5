dd80234b6f687122ddb72cd3dc25b2503bf3114a
Oh Ga Eun <dhrkdms4113@naver.com>
2023-04-03 15:47:26 +0900

    Printing to the screen using JSON

---
+++ JavaScript_/230403/useCases.js
@@ -0,0 +1,81 @@
+// step1
+fetch('http://test.api.weniv.co.kr/mall')
+  .then(data => data.json())
+  .then(data => console.log(data))
+
+// step2
+fetch('http://test.api.weniv.co.kr/mall')
+  .then(data => data.json())
+  .then(data => {
+    data.forEach(item => {
+      console.log(item)
+    })
+  })
+// step3
+fetch('http://test.api.weniv.co.kr/mall')
+  .then(data => data.json())
+  .then(data => {
+    data.forEach(item => {
+      console.log(item.thumbnailImg)
+      console.log(item.productName)
+      console.log(item.price)
+    })
+  })
+// step4
+fetch('http://test.api.weniv.co.kr/mall')
+  .then(data => data.json())
+  .then(data => {
+    data.forEach(item => {
+      console.log('http://test.api.weniv.co.kr/' + item.thumbnailImg)
+      console.log(item.productName)
+      console.log(item.price)
+    })
+  })
+// step5 : DOM 생성
+fetch('http://test.api.weniv.co.kr/mall')
+  .then(data => data.json())
+  .then(data => {
+    const main = document.createElement("main")
+    data.forEach(item => {
+      // console.log('http://test.api.weniv.co.kr/' + item.thumbnailImg)
+      // console.log(item.productName)
+      // console.log(item.price)
+      const productCard = document.createElement('article')
+      // const productimg = document.createElement('img')
+      const productName = document.createElement('h2')
+      const productPrice = document.createElement('p')
+
+      productName.textContent = item.productName
+      productPrice.textContent = item.price
+      productCard.appendChild(productName)
+      productCard.appendChild(productPrice)
+
+      main.appendChild(productCard)
+    })
+    document.body.appendChild(main)
+  })
+// step6 : 화면 출력
+fetch('http://test.api.weniv.co.kr/mall')
+  .then(data => data.json())
+  .then(data => {
+    const main = document.createElement("main")
+    data.forEach(item => {
+      // console.log('http://test.api.weniv.co.kr/' + item.thumbnailImg)
+
+      const productCard = document.createElement('article')
+      const productimg = document.createElement('img')
+      const productName = document.createElement('h2')
+      const productPrice = document.createElement('p')
+
+      productName.textContent = item.productName
+      productPrice.textContent = item.price
+      productimg.setAttribute('src', 'http://test.api.weniv.co.kr/' + item.thumbnailImg)
+      // productimg.attributes
+      productCard.appendChild(productimg)
+      productCard.appendChild(productName)
+      productCard.appendChild(productPrice)
+
+      main.appendChild(productCard)
+    })
+    document.body.appendChild(main)
+  })
