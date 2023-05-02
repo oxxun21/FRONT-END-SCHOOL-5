01226430715f95446462c322c92f84b0eb71f67b
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-02 11:32:10 +0900

    Vending Machine 구현 완성

--- JavaScript_/vending/js/classes/vendingMachineEvents.js
+++ JavaScript_/vending/js/classes/vendingMachineEvents.js
@@ -18,8 +18,10 @@ class VendingMachineEvents {
   }
 
   // 장바구니 콜라 생성 함수
-  stagedItemGenerator(target){
+  stagedItemGenerator(target) {
     const stagedItem = document.createElement('li');
+    stagedItem.dataset.item = target.dataset.item;
+    stagedItem.dataset.price = target.dataset.price;
     stagedItem.innerHTML = `
       <img src="./img/${target.dataset.img}" alt="">
       ${target.dataset.item}
@@ -30,7 +32,7 @@ class VendingMachineEvents {
     this.stagedList.append(stagedItem);
   }
 
-  bindEvent(){
+  bindEvent() {
     /**
     * 1. 입금 버튼 기능
     * 입금 버튼을 누르면 
@@ -39,22 +41,22 @@ class VendingMachineEvents {
     * 3) 입금액이 소지금보다 많으면 경고창 출력
     * 4) 입금액이 정상적으로 입금되면 입금창은 초기화
     */
-    this.btnPut.addEventListener('click', () => {   
+    this.btnPut.addEventListener('click', () => {
       const inputCost = parseInt(this.inputCostEl.value); // 입력값
       const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(',', ''));  // 소지금
       const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));  // 잔액
 
-      if (inputCost) {      
+      if (inputCost) {
         if (inputCost <= myMoneyVal && inputCost > 0) { // 입금액이 소지금 보다 적거나 같다면
           this.myMoney.textContent = new Intl.NumberFormat().format(myMoneyVal - inputCost) + '원';
-          this.balance.textContent = new Intl.NumberFormat().format((balanceVal ? balanceVal : 0) + inputCost) + '원';         
+          this.balance.textContent = new Intl.NumberFormat().format((balanceVal ? balanceVal : 0) + inputCost) + '원';
         } else {  // 입금액이 소지금보다 많다!
           alert('소지금이 부족합니다.');
         }
 
         this.inputCostEl.value = null;
       }
-    })
+    });
 
     /**
     * 2. 거스름돈 반환 버튼
@@ -65,11 +67,11 @@ class VendingMachineEvents {
       const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(',', ''));  // 소지금
       const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));  // 잔액
 
-      if(balanceVal){
+      if (balanceVal) {
         this.myMoney.textContent = new Intl.NumberFormat().format(balanceVal + myMoneyVal) + '원';
         this.balance.textContent = null;
       }
-    })
+    });
 
     /**
     * 3. 자판기 장바구니 채우기
@@ -79,24 +81,86 @@ class VendingMachineEvents {
     * 4) 아이템의 count가 -1이 됩니다. 아이템의 카운트가 0이되면 품절 표시를 합니다.
     */
     this.btnsCola = document.querySelectorAll('.section1 .btn-cola');
+
     this.btnsCola.forEach((item) => {
       item.addEventListener('click', (event) => {
-        const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));  // 잔액
-        const targetElPrice = parseInt(event.currentTarget.dataset.price);
-        
-        if(balanceVal >= targetElPrice){
+        const balanceVal = parseInt(this.balance.textContent.replaceAll(',', ''));
+        const targetEl = event.currentTarget
+        const targetElPrice = parseInt(targetEl.dataset.price);
+        const stagedListitem = this.stagedList.querySelectorAll('li');
+        let isStaged = false
+
+        if (balanceVal >= targetElPrice) {
           this.balance.textContent = new Intl.NumberFormat().format(balanceVal - targetElPrice) + '원';
 
-          // 장바구니 콜라 생성
-          this.stagedItemGenerator(event.currentTarget);
-          for(const item of this.stagedList){
-            
+          //장바구니 콜라 생성
+          for (const item of stagedListitem) {
+            // 클릭한 콜라의 이름과 장바구니에 있던 콜라의 이름이 같은지 비교!
+            if (targetEl.dataset.item === item.dataset.item) {
+              item.querySelector('strong').firstChild.textContent = parseInt(item.querySelector('strong').firstChild.textContent) + 1;
+
+              isStaged = true;
+              break
+            }
+          }
+
+          // 처음 선택했을 경우에만 장바구니에 콜라를 생성한다
+          if (!isStaged) {
+            this.stagedItemGenerator(event.currentTarget);
+          }
+          //자판기 콜라 개수 차감
+          targetEl.dataset.count--;
+
+          if (!parseInt(targetEl.dataset.count)) {
+            targetEl.insertAdjacentHTML('beforeend', `
+              <strong class="soldout">
+                <span>품절</span>
+              </strong>
+              `
+            );
+
+            targetEl.disabled = "disabled"
           }
         } else {
-          alert('입금액이 부족합니다.');
+          alert('입금한 금액이 부족합니다.');
         }
       })
-    })
+    });
+
+    /**
+    * 4. 획득 버튼 기능
+    * 1) 장바구나에 있는 음료수 목록이 획득한 음료로 이동
+    * 2) 획득한 음료의 모든 금액을 합하여 총 금액을 업데이트
+    */
+    this.btnGet.addEventListener('click', () => {
+      const itemStagedList = this.stagedList.querySelectorAll('li');
+      const itemGetList = this.getList.querySelectorAll('li');
+      let totalPrice = 0;
+      
+      for(const itemStaged of itemStagedList){
+        let isGet = false; // 이미 획득 했나?
+        for(const itemGet of itemGetList){
+          // 장바구니의 콜라가 이미 획득한 목록에 있다면
+          if(itemStaged.dataset.item === itemGet.dataset.item){
+            itemGet.querySelector('strong').firstChild.textContent = parseInt(itemGet.querySelector('strong').firstChild.textContent) + parseInt(itemStaged.querySelector('strong').firstChild.textContent);
+
+            isGet = true;
+            break;
+          }
+        }
+        if (!isGet){
+          this.getList.append(itemStaged);
+        }
+      }
+
+      this.stagedList.innerHTML = null;
+
+      // 획득한 음료 리스트의 총 금액 계산
+      this.getList.querySelectorAll('li').forEach((itemGet) => {
+        totalPrice += parseInt(itemGet.dataset.price) * parseInt(itemGet.querySelector('strong').firstChild.textContent);
+      });
+      this.txtTotal.textContent = `총금액 : ${new Intl.NumberFormat().format(totalPrice)} 원`
+    });
   }
 }
 
