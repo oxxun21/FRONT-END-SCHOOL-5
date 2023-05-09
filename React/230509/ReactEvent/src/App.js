cc7e7e359dad3cbc2c9a960cd1d016ca45501ce3
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-09 15:52:07 +0900

    9. 컴포넌트 리스트 만들기

--- React/230509/ReactEvent/src/App.js
+++ React/230509/ReactEvent/src/App.js
@@ -3,9 +3,23 @@ import Login from './components/Login/Login'
 import Homepage from './components/Login/Homepage';
 import Modal from './components/Modal/Modal';
 
+function Hello(props) {
+	const name = props.name;
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //1~10호를 만들 것입니다.
+	
+  // list map을 이용하여 h1 태그들의 리스트를 만듭니다.
+  const numComponentsArray = num.map((i)=><h1>안녕, {name} {i}호</h1>);
+
+  return(
+    <div>
+      {numComponentsArray}
+    </div>
+  )
+}
+
 export default function App() {
   const [login, setLogin] = useState(false);
-  const [modal, setModal] = useState(true);
+  const [modal, setModal] = useState(false);
 
   const closeModal = () => {
     setModal(false);
@@ -22,6 +36,7 @@ export default function App() {
 
   return (
     <>
+      <Hello />
       {login ? <Homepage /> : <Login user={user} setLogin={setLogin} />}
       <button type="button" onClick={openModal}>열기</button>
       {modal && <Modal closeModal={closeModal}>
