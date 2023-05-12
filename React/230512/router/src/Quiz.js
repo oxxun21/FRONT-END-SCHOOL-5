b0a19be336295f1e650a01a14cdad0c1a63bae25
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-12 15:58:43 +0900

    13. React Router

---
+++ React/230512/router/src/Quiz.js
@@ -0,0 +1,20 @@
+// # Quiz - Route 사용하여 페이지 경로 만들기
+
+// - **Home Page :** /
+// - **Product Detail Page** : /products/:id
+//     - ex) /products/1 , /products/2, /products/3, /products/4
+// - **Product Detail Notice Page :** /products/:id/notice
+//     - ex) /products/1/notice , /products/2/notice…
+// - **Cart Page :** /cart
+// - **Coupon Page :** /users/coupon
+// - **Question Page :** /users/question
+// - **Notice Page :** /users/notice
+// - **User Page :** /users
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
