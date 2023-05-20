b123c4aec25a161f7e793bc72fa8bf26852e592b
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-20 21:01:39 +0900

    commit

---
+++ JavaScript_/230520_Class/app.js
@@ -0,0 +1,10 @@
+// 기본 캔버스 설정
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext('2d');
+const cWidth = canvas.width;
+const cHeight = canvas.height;
+
+// bg 캔버스 설정
+const canvasBg = document.querySelector("#canvasBg");
+const ctxBg = canvasBg.getContext('2d');
+
