a9fa8c3aef9d26c8c110fb84db2ce68e23feed96
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-30 15:06:37 +0900

    알고리즘 (3)

--- DStructureAlgo/bubbleSort.js
+++ DStructureAlgo/bubbleSort.js
@@ -6,10 +6,10 @@ let d = [4, 3, 2, 1]// [3,4,2,1] ==> [3,2,4,1] ==> [3,2,1,4] 비교 : 3
 
 let myArr = [10, 9, 8, 5, 6, 1, 3, 4, 2, 1];
 
-function bubbleSort(arr){
-  for(let i = 0; i < arr.length - 1; i++){
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
     // 원소 위치 변경
-    if(arr[i] > arr[i + 1]){
+    if (arr[i] > arr[i + 1]) {
       // let temp = arr[i];
       // arr[i] = arr[i + 1];
       // arr[i + 1] = temp;
@@ -22,10 +22,10 @@ function bubbleSort(arr){
 
 let result = bubbleSort(myArr);
 
-function bubbleSortAdvanced(arr){
-  for(let i = 0; i < arr.length - 1; i++){
-    for(let j = 0; j < arr.length - 1; j++){
-      if(arr[j] > arr[j + 1]){
+function bubbleSortAdvanced(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
     }
@@ -34,3 +34,27 @@ function bubbleSortAdvanced(arr){
 }
 
 let result2 = bubbleSortAdvanced(myArr);
+
+function bubbleSortAdvanced2(arr) {
+  let counter = 0;
+  for (let index = 0; index < arr.length - 1; index++) {
+    // 정렬이 필요한 배열인지 상태를 저장합니다.
+    let isSorted = true;
+    for (let index2 = 0; index2 < arr.length - 1 - index; index2++) {
+      // 원소의 위치를 변경해줍니다.
+      if (arr[index2] > arr[index2 + 1]) {
+
+        [arr[index2], arr[index2 + 1]] = [arr[index2 + 1], arr[index2]];
+        isSorted = false;
+      }
+      console.log(arr);
+      counter++;
+    }
+
+    if (isSorted) break;
+  }
+  console.log('counter :', counter);
+  return arr
+}
+
+bubbleSortAdvanced2(myArr);
